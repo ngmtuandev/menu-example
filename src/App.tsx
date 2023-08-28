db53badbf531a1e243a2c1dd64e870f886bdfb87
ngmtuandev <nguyenmanhtuancomputer@gmail.com>
2023-08-28 08:09:22 +0700

    version1

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Type from "./Type";
 import Menus from "./Menus";
 import data from "./data";
+import "./index.css";
 function App() {
   const type = ["all", "breakfast", "lunch", "shakes"];
   const [typeCurrent, setTypeCurrent] = useState("all");
@@ -18,17 +19,26 @@ function App() {
     }
   }, [typeCurrent]);
   return (
-    <div>
-      <div>
+    <div className="flex flex-col justify-center items-center mt-10">
+      <h3
+        className="text-yellow-400 uppercase font-semibold text-[25px] border-b-2 border-yellow-400
+       w-[140px] flex justify-center items-center"
+      >
+        Our Menu
+      </h3>
+      <div className="flex">
         {type.map((item, index) => {
           return (
-            <div key={index}>
+            <div
+              key={index}
+              className="mx-3 px-2 py-1 bg-yellow-400 text-gray-50 font-semibold rounded-md hover:bg-opacity-80 cursor-pointer my-6"
+            >
               <Type onSetType={setTypeCurrent} type={item}></Type>
             </div>
           );
         })}
       </div>
-      <div>
+      <div className="flex justify-center items-center">
         <Menus data={listDataForType}></Menus>
       </div>
     </div>
--- src/Menus.tsx
+++ src/Menus.tsx
@@ -2,16 +2,31 @@ import React from "react";
 
 const Menus = ({ data }) => {
   console.log("data : ", data);
+
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-5">
       {data.length > 0 &&
         data.map((item) => {
           return (
-            <div key={item.id}>
-              <span>name: {item.title}</span>
-              <br />
-              <span>category: {item.category}</span>
-              <div>------</div>
+            <div key={item.id} className="flex mb-5 w-[300px] flex-col">
+              <img
+                src={item.img}
+                className="w-[300px] rounded-md shadow-xl h-[200px]"
+                alt=""
+              />
+              <div className="flex my-3 items-center">
+                <span className="mr-3 text-gray-800 font-semibold text-[22px]">
+                  {item.title}
+                </span>
+                <div className="px-2 py-1 bg-yellow-400 rounded-md text-gray-100 font-medium">
+                  {item.price}
+                </div>
+              </div>
+              <div>
+                <span className="text-gray-600 font-normal text-[15px]">
+                  {item.desc}
+                </span>
+              </div>
             </div>
           );
         })}
